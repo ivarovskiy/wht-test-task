8c5b93d35676d22fb83d841bdc901663b9108991
Ivarovskiy <ivarovskiy00@gmail.com>
2023-08-17 22:56:46 +0300

    trouble with pages, try to log errors

--- src/app/services/cat-api.service.ts
+++ src/app/services/cat-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, catchError, map, throwError } from 'rxjs';
 import { ICat } from '../models/cat';
 import { Store } from '@ngxs/store';
 
@@ -21,18 +21,25 @@ export class CatApiService {
     const params = new HttpParams().set('api_key', this.apiKey);
 
     const url = `${this.apiBaseUrl}/images/${id}`;
-    return this.http.get<ICat>(url, { params });
+    return this.http.get<ICat>(url, { params }).pipe(
+      catchError(error => {
+        console.error('Error fetching cat by ID:', error);
+        return throwError('Failed to fetch cat by ID');
+      })
+    );
   }
 
   fetchBreedNames(): Observable<any[]> {
     const params = new HttpParams().set('api_key', this.apiKey);
 
     const url = `${this.apiBaseUrl}/breeds`;
-    return this.http
-      .get<any[]>(url, { params })
-      .pipe(
-        map(breeds => breeds.map(breed => ({ id: breed.id, name: breed.name })))
-      );
+    return this.http.get<any[]>(url, { params }).pipe(
+      map(breeds => breeds.map(breed => ({ id: breed.id, name: breed.name }))),
+      catchError(error => {
+        console.error('Error fetching breed names:', error);
+        return throwError('Failed to fetch breed names');
+      })
+    );
   }
 
   fetchCatsFromApi(
@@ -53,6 +60,11 @@ export class CatApiService {
     }
 
     const url = `${this.apiBaseUrl}/images/search`;
-    return this.http.get<ICat[]>(url, { params });
+    return this.http.get<ICat[]>(url, { params }).pipe(
+      catchError(error => {
+        console.error('Error fetching cats from API:', error);
+        return throwError('Failed to fetch cats from API');
+      })
+    );
   }
 }
